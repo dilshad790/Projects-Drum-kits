4eb6e6f9fac2671220dca2283c2bd389bf61050f
Md. Dilshad Alam <93848536+dilshad790@users.noreply.github.com>
2023-08-06 11:36:20 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,51 @@
 
+var numberOfDrum=document.querySelectorAll(".drum").length;
+
+document.querySelectorAll(".drum")[0].addEventListener("click",function (){
+    var audio=new Audio("sounds/tom-1.mp3")
+    audio.play();
+
+    // console.log(this.innerHTML);
+});
+
+document.querySelectorAll(".drum")[1].addEventListener("click",function (){
+    var audio=new Audio("sounds/tom-2.mp3")
+    audio.play();
+
+    // console.log(this.innerHTML);
+});
+
+document.querySelectorAll(".drum")[2].addEventListener("click",function (){
+    var audio=new Audio("sounds/tom-3.mp3")
+    audio.play();
+
+    // console.log(this.innerHTML);
+});
+
+document.querySelectorAll(".drum")[3].addEventListener("click",function (){
+    var audio=new Audio("sounds/tom-4.mp3")
+    audio.play();
+
+    // console.log(this.innerHTML);
+});
+
+document.querySelectorAll(".drum")[4].addEventListener("click",function (){
+    var audio=new Audio("sounds/crash.mp3")
+    audio.play();
+
+    // console.log(this.innerHTML);
+});
+
+document.querySelectorAll(".drum")[5].addEventListener("click",function (){
+    var audio=new Audio("sounds/kick-bass.mp3")
+    audio.play();
+
+    // console.log(this.innerHTML);
+});
+
+document.querySelectorAll(".drum")[6].addEventListener("click",function (){
+    var audio=new Audio("sounds/snare.mp3")
+    audio.play();
+
+    // console.log(this.innerHTML);
+});
